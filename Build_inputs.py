8ef6199582f97ff708eb849fece57e8e8e8e107d
KCharoenpitaks <50170318+KCharoenpitaks@users.noreply.github.com>
2019-12-29 10:51:09 +0700

    Create Build_inputs.py

---
+++ Build_inputs.py
@@ -0,0 +1,17 @@
+    def _build_inputs(self, batch, t):
+        # Assumes homogenous agents with flat observations. #This is Key because it shares the same NN
+        # Other MACs might want to e.g. delegate building inputs to each agent
+        bs = batch.batch_size
+        #print("bs=",bs)
+        inputs = []
+        inputs.append(batch["obs"][:, t])  # b1av
+        if self.args.obs_last_action:
+            if t == 0:
+                inputs.append(th.zeros_like(batch["actions_onehot"][:, t]))
+            else:
+                inputs.append(batch["actions_onehot"][:, t-1])
+        if self.args.obs_agent_id:
+            inputs.append(th.eye(self.n_agents, device=batch.device).unsqueeze(0).expand(bs, -1, -1))
+
+        inputs = th.cat([x.reshape(bs*self.n_agents, -1) for x in inputs], dim=1)
+        return inputs
